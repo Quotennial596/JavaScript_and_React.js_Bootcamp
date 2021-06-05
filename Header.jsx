320267621f76d5576c2e1a21747958563b16bfe5
Quotennial596 <71747420+Quotennial596@users.noreply.github.com>
2021-06-05 22:08:09 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <h1>Priyanshu Darji</h1>
+        </header>
+    );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return (
+       <div className = "note">
+           <h1>javascript and React.js</h1>
+           <p>“You should imagine variables as tentacles, rather than boxes. They do not contain values; they grasp them—two variables can refer to the same value.” “You're building your own maze, in a way, and you might just get lost in it.” “jQuery is by far the most widely used library for JavaScript.</p>
+       </div>
+    );
+}
+
+export default Note;
